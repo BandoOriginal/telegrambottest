452ea0100a6d60b12121b67d42d55ade2d7a85bb
BandoOriginal <bandooriginal@yahoo.com>
2025-01-14 22:38:10 -0400

    API Get command

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express();
 //Rutas de la API
 app.post("*", (req, res) => {
   console.log(req.body)
+  console.log("Si se hizo POST")
   res.send("Hello Post")
 })
 app.get("*", (req, res) => {
   console.log(req.body)
+  console.log("Si se hizo GET")
   res.send("Hello GET")
 })
 
